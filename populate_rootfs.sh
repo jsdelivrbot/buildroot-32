4ff6d61805eee6f0e856564fdcc5d9776d221f32
Kamil Lulko <kamil.lulko@gmail.com>
2016-09-22 23:23:50 +0200

    fix populate rootfs script

--- populate_rootfs.sh
+++ populate_rootfs.sh
@@ -3,6 +3,6 @@ rm -rf /home/varcain/tftp/*
 rm -rf /home/varcain/nfs/*
 cp /home/varcain/projects/buildroot/output/images/zImage /home/varcain/tftp/
 cp /home/varcain/projects/buildroot/output/images/bcm2709-rpi-2-b.dtb /home/varcain/tftp/rpi2.dtb
-tar -xf /home/varcain/projects/buildroot/output/images/rootfs.tar -C /home/varcain/nfs/
+fakeroot tar --no-overwrite-dir -xf /home/varcain/projects/buildroot/output/images/rootfs.tar -C /home/varcain/nfs/
 echo "Done!"
 
